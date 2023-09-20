b803591ae0b83f684a3ee841933477a370f97661
Владислав Александрович Барсуков <destin_835@mail.ru>
2023-09-21 01:14:54 +0500

    Практика номер 4

---
+++ PZ_04/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace PZ_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вариант номер 5
+            //Первое задание
+            //Console.WriteLine("Введите число: (60)");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //for (int b = 0; b <= a; b+=2)
+            //    Console.WriteLine(b);
+
+            //Второе задание
+            //Console.WriteLine("Введите количество букв");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите букву (e 'en')");
+            //var b = char.ToLower(Console.ReadKey().KeyChar);
+            //for (var i = 1; i <= a; i++)
+            //{
+            //    b = b == 'z' ? 'a' : (char)(b + 1);
+            //    Console.WriteLine(b);
+            //Третье задание
+            //Console.WriteLine("Введите количество символов");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите количество строк");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //for (var c = 0; c < b; c++)
+            //{
+            //    for (var d = 0; d < a; d++)
+            //    {
+            //        Console.Write('#');
+            //    }
+            //    Console.WriteLine();
+            //Четвертое задание
+            //Console.WriteLine("Введите значение кратности");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //for (int b = 0; b <= 1000; b++)
+            //{
+            //    if (b % a == 0)
+            //        Console.WriteLine(b);
+            //Пятое задание
+            //Console.WriteLine("Введите разницу чисел (18)");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //for (int b = 2, c = 40; Math.Abs(b - c) != a; b++, c--)
+            //{
+            //    if (b + c <= 18)
+            //        break;
+            //    Console.WriteLine("{0} {1}", b, c);
+            //}
+            //5 задание работает, но не так как нужно, все перепробовал, но такой вариант самый адекватный
+        }
+    }
+}
