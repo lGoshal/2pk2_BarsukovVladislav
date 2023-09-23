7a9dda44a40e1676e1dec55c2d7f4da05d238e6e
Владислав Александрович Барсуков <destin_835@mail.ru>
2023-09-23 14:14:56 +0500

    Практика номер 5

---
+++ PZ_05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+
+namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine("Введите максимальное значение переменной (50)");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной (2)");
+            int c = Convert.ToInt32(Console.ReadLine());
+            while (c <= a)
+            {
+                Console.WriteLine(c);
+                c *= b;
+            }
+            //Надеюсь это не возведение в степень), работает не только для 2 но и для других значений.
+        }
+    }
+}
