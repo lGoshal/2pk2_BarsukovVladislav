c5cd5e57bfb967fc3630ce01774513720a702382
Владислав Александрович Барсуков <destin_835@mail.ru>
2023-12-05 01:52:09 +0500

    Практика номер 14

---
+++ PZ_14/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = new int[5, 8];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    A[i, j] = i * j;
+                }
+            }
+
+            FileStream file = new FileStream(@"D:\test.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(file); 
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        writer.Write(A[i, j] + " ");
+                    }
+                    writer.WriteLine();
+                }
+            }
+            writer.Close();
+            Console.WriteLine("Массив успешно записан в файл: test.txt");
+        }
+    }
+}
---
+++ PZ_15/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название директории: ");
+            string dirName = Console.ReadLine();
+            if (Directory.Exists(dirName))//Проверка на существование директории
+            {
+                Console.WriteLine("Подкаталоги: ");
+                string[] dirs = Directory.GetDirectories(dirName);//Вывод всех папок в директории
+                if (dirs.Length == 0)
+                    Console.WriteLine("Каталогов нет");
+                foreach (string s in dirs)//Вывод папок
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine("Файлы: ");//Вывод файлов
+            string[] files = Directory.GetFiles(dirName);
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Файлов нет: ");
+            }
+            else
+            {
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine("Укажите название файла: ");//Ввод названия папки
+                string FileName = Console.ReadLine();
+                FileStream file1 = new FileStream($@"{FileName}.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(file1);
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+            }
+        }
+    }
+}
